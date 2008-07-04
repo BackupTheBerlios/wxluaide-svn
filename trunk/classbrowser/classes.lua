7c18553b54f7ec85e17398faa0d5e4e5cc5a320f
dsacks <dsacks@68a01eaa-ed4d-0410-99a4-a233e5fdb40c>
2008-07-04 00:36:44 +0000

    worked on classes and db

--- trunk/classbrowser/classes.lua
+++ trunk/classbrowser/classes.lua
@@ -1,5 +1,7 @@
 -- classes for codebrowser
+package.cpath = package.cpath..";./?.dll;./?.so;../lib/?.so;../lib/vc_dll/?.dll;../lib/bcc_dll/?.dll;../lib/mingw_dll/?.dll;lib/?.dll;/usr/local/luaaio/lib/?.so"
 require 'class'
+require 'lib/luasqlite3'
 
 Project = class(function(p,name,baseDir,srcDir)
     p.name = name
@@ -13,3 +15,26 @@ Project = class(function(p,name,baseDir,srcDir)
 function Project:__tostring()
     return self.name
 end
+
+ProjMgr = class(function(pm)
+    --[[
+    ProjMgr is a class to manage, load and save projects
+    
+    ]]
+    
+   
+    end)
+
+    
+function ProjMgr:loadProject(name)
+
+
+end
+    
+-- db logic
+Database = class(function(db)
+    db.location = "./cbrowser.sql3"
+    db.handle = sqlite3.open(db.location)
+    
+    end)
+    
---
+++ trunk/classbrowser/createdb.lua
@@ -0,0 +1,10 @@
+package.cpath = package.cpath..";./?.dll;./?.so;../lib/?.so;../lib/vc_dll/?.dll;../lib/bcc_dll/?.dll;../lib/mingw_dll/?.dll;lib/?.dll;/usr/local/luaaio/lib/?.so"
+require 'lib/luasqlite3'
+
+db = sqlite3.open("cbrowser.sqlite3")
+
+db:exec[[ 
+    create table project (id, name, baseDir,srcDir,classes,srcFiles,packages,type);
+    create table package (id, projectid, package,code);
+ 
+ ]]
